a87e2d72d75565f3a64e4052dcbf46d155bab39c
Gustavo <gustavosr98@gmail.com>
2018-09-12 01:04:21 -0400

    GameOver Trigger
    En la escena Level1
    - Se agregaron cinco GameObjects vacios, uno llamado Walls que solo sirve para organizar y contiene a los demas. Otros tres que son Left, Right y Top Wall a las que se les agrego un Box Collider 2D para que sirvan de choque para la pelota.
    **Y uno ultimo que se llama GameOver  Wall al cual se le agrego tambien el Box Collider 2D pero ademas se le activo la opcion de "is Trigger" para que se ejecute el GameOver cuando algo choca con este collider.
    ** Tambien se le agrego como componente un script llamado GameOverWall y se sobreescribio un metodo de Unity que se ejecuta cuando algo choca con este objeto.
    
    - Se agregaron dos metodos mas al script de SceneLoader
    
    Se creo una nueva escena llamada GameOver
    - Se agrego esta escena al BuildSettings como ultima escena.
    - Esta escena tiene copiados y pegados los GameObjects de la escena Menu, modificando el titulo y en el boton se cambia el metodo que ejecuta al presionarse a LoadMainMenu()

---
+++ Brick Breaker/Assets/Scripts/GameOverWall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverWall : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneLoader sceneLoader = new SceneLoader();
+        sceneLoader.LoadGameOver();
+    }
+}
--- Brick Breaker/Assets/Scripts/SceneLoader.cs
+++ Brick Breaker/Assets/Scripts/SceneLoader.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
 
-	public void LoadFirstLevel()
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadFirstLevel()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene(SceneManager.sceneCountInBuildSettings - 1);       
+    }
+    
 }
