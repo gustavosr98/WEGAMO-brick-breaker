66bd3437bbe20fcfc5ccea3d9f8f471778a2447d
Gustavo <gustavosr98@gmail.com>
2018-09-12 00:28:58 -0400

    Input en la Barra
    Que la barra siga la posicion en X del mouse de la computadora
    
    - Se agrega el script Player como componente del GameObject Player. (La barra)
    
    NOTA: Se divide entre Screen.width y multiplica por screenWidthInUnits para escalar la posicion del mouse obtenida en pixeles (que depende del tamaño de la pantala) a unidades de Unity usadas en el juego.
    
    NOTA2: [SerializeField] se utiliza para observar los atributos de un script desde el Inspector de Unity y puedan ser editados tambien a mano desde ahi.

---
+++ Brick Breaker/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float screenWidthInUnits = 16f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();	
+	}
+
+    //Por defecto los metodos son privados
+    void Move()
+    {
+        Vector2 barPosition = new Vector2(Input.mousePosition.x / Screen.width * screenWidthInUnits, transform.position.y);
+        transform.position = barPosition;
+    }
+}
